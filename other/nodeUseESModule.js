96e221c7f98ffcf2913a97894c32c3710f27349d
随步 <suibu@qunhemail.com>
2021-05-03 18:25:00 +0800

    A: use es module in node env

--- other/nodeUseESModule.js
+++ other/nodeUseESModule.js
@@ -1,4 +1,6 @@
 // Node 中支持 es module， node默认支持commonjs规范
+// 方法一： 配置webpack，babel 配置编译：useESModules
+// 方法二：node --experimental-modules **.mjs 或者是 版本升到v14，node **.mjs, 到14+之后默认支持esModule，但是文件类型需要是mjs
 
 // 模块化： AMD CMD require.js
 // CommonJS： 通过require加载模块，通过module.exports / exports.xx 暴露模块
@@ -16,6 +18,3 @@ console.log(isExist(path.resolve('.')))
   })
 })()
 
-// Node 中支持 es module
-// 方法一： 配置webpack，babel 配置编译：useESModules
-// 方法二：node --experimental-modules **.mjs 或者是 版本升到v14，node **.mjs, 到14+之后默认支持esModule，但是文件类型需要是mjs
